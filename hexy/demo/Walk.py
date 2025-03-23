2cc9105125c40e5c9c71ff4f81c9204fd7c8be23
Rory Steel <rory.steel@gmail.com>
2025-03-23 17:17:49 +0000

    walk2

--- hexy/demo/Walk.py
+++ hexy/demo/Walk.py
@@ -6,8 +6,14 @@ hexy =  Hexapod()
 print("lie flat")
 hexy.lie_flat()
 
+print("get up")
+hexy.get_up()
+
 print("walk forward")
 hexy.walk(offset = 25, swing = 25)
+
+print("squat")
+hexy.squat()
 
 print("lie flat")
 hexy.lie_flat()
